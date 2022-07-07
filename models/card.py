3d033a555697864ce578ec58cef80c2bfc4833ee
Mario <193254@ids.upchiapas.edu.mx>
2022-07-06 20:18:31 -0500

    Cambios en la creacion de la tabla Card

--- models/card.py
+++ models/card.py
@@ -7,7 +7,10 @@ card = Table(
     Meta,
     Column("id", Integer, primary_key=True, autoincrement=True),
     Column("name",String(255)),
-    Column("description",String(255))
+    Column("description",String(255)),
+    Column("Id-tc", Integer, ForeignKey("TipoClase.id")),
+    Column("Id-R", Integer,  ForeignKey("rareza.id")),
+    Column("Id-TClass", Integer,  ForeignKey("TipoCarta.id"))
 )
 
 Meta.create_all(engine)
